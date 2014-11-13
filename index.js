8857b7a9862ee540c92b7b4a6a21f9a4113a760f
xiangshouding <fansekey@gmail.com>
2014-11-13 16:19:40 +0800

    add

---
+++ index.js
@@ -0,0 +1,31 @@
+var _ = require('lodash');
+var util = require('util');
+var path = require('path');
+var log = require('./lib/log.js')();
+
+function Scaffold (options) {
+    this._options = _.merge({}, options);
+    _.call(this);
+}
+
+util.inherits(Scaffold, _);
+
+Scaffold.prototype.download = function (id, cb) {
+    log.notice('will download component id: ' + id);
+    if (!id) {
+        log.error(new Error('invalid, '));
+    }
+    var supports = ['github', 'gitlab', 'lights'];
+    var type = this._options['type'];
+    if (!~supports.indexOf(type)) {
+        type = 'lights';
+    }
+    var request = new (require('./lib/'+type))(this._options);
+    log.notice(type + ': download start');
+    request.download(id, cb || function(){});
+};
+
+Scaffold.prototype.prompt = function (id) {
+};
+
+module.exports = Scaffold;
