4c375c92bf4f6338b59e8ac82c94c1bd6bfd0fae
xiangshouding <fansekey@gmail.com>
2014-11-14 22:24:06 +0800

    ut

--- test/ut/util.js
+++ test/ut/util.js
@@ -2,8 +2,41 @@ var chai = require('chai');
 var expect = chai.expect;
 var assert = chai.assert;
 
-var util = require('../../lib/util.js');
+var fs = require('fs');
+var _ = require('../../lib/util.js');
 
 //console.log(util.find('../../', null, /\.js/));
 
 //util.copy('/Users/shouding/Downloads/fisp-lsdiff-demo', '/Users/shouding/Downloads/test',null,null,true);
+
+
+describe('_.del', function () {
+    var p = __dirname + '/.tmp';
+    before(function () {
+        fs.mkdirSync(p);
+    });
+
+    after(function() {
+        fs.rmdirSync(p);
+    });
+
+    it('_.del a file', function () {
+        var f = p + '/del_1.log';
+        fs.writeFileSync(f, 'test');
+        expect(fs.existsSync(f)).to.be.equal(true);
+        _.del(f);
+        expect(fs.existsSync(f)).to.be.equal(false);
+    });
+
+    it ('_.del a dir', function () {
+        var p1 = p + '/del_2';
+        fs.mkdirSync(p1);
+        expect(fs.existsSync(p1)).to.be.equal(true);
+        _.del(p1);
+        expect(fs.existsSync(p1)).to.be.equal(false);
+    });
+
+    it ('_.del ..', function () {
+
+    });
+});
